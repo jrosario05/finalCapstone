e23c15453841dff28a76a82e139b2474878796cc
Michael Doom <michael.l.doom@gmail.com>
2022-12-02 23:53:10 -0500

    some vue stuff

---
+++ vue/src/components/BreweryDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <p>SHOW BREWERY DETAILS</p>
+      <beer-cards/>
+  </div>
+</template>
+
+<script>
+import BeerCards from './BeerCards.vue'
+export default {
+name: 'breweries-details',
+
+components: { BeerCards },
+
+}
+</script>
+
+<style>
+
+</style>
