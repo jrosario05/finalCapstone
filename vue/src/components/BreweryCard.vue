e734b1641f6fe44fbd2b2a1055dddc7cdd9439cb
Michael Doom <michael.l.doom@gmail.com>
2022-12-03 22:45:09 -0500

    added bootstrap

--- vue/src/components/BreweryCard.vue
+++ vue/src/components/BreweryCard.vue
@@ -17,7 +17,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 div {
   color: white;
 }
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+   <p>You must be authenticated to see this</p>
   </div>
 </template>
 
@@ -10,3 +10,10 @@ export default {
   name: "home"
 };
 </script>
+
+<style>
+div{
+  color: white;
+}
+
+</style>
