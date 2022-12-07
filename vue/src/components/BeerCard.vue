9229330619157ff6779f0944548378e0b6a2c43f
Andrew Brantley <abrantl@gmail.com>
2022-12-07 13:21:34 -0500

    first pass at passport layout

--- vue/src/components/BeerCard.vue
+++ vue/src/components/BeerCard.vue
@@ -1,3 +1,5 @@
+/* v-show="$store.state.token != ''" */
+
 <template>
   <div>
           
---
+++ vue/src/views/Passport.vue
@@ -0,0 +1,15 @@
+<template>
+  <my-passport/>
+</template>
+
+<script>
+import MyPassport from '../components/Passport.vue';
+
+export default {
+  name: "passport",
+  components: {MyPassport},
+ 
+}
+</script>
+<style>
+</style>
