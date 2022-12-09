486c4766565f9d4e26da2b57453e5f58fbc739e3
Joel Rosario <aurelioros05@gmail.com>
2022-12-09 10:38:54 -0500

    pulling

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -157,6 +157,15 @@ export default {
 
 <style scoped> 
 
+body {
+   background: url("https://www.patriot-place.com/wp-content/uploads/2019/09/Wormtown-beer-garden-hero-bg.jpg") no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 100vh;
+  background-color: black;
 
+}
 
 </style>
