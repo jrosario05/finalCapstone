6cc9837d1ee93f0d312eb8e7d5574c606befb073
Joel Rosario <aurelioros05@gmail.com>
2022-12-14 15:15:03 -0500

    pulling

--- java/src/test/java/com/techelevator/dao/JdbcBeerDaoTest.java
+++ java/src/test/java/com/techelevator/dao/JdbcBeerDaoTest.java
@@ -34,7 +34,7 @@ public class JdbcBeerDaoTest extends BaseDaoTests{
     @Test
     public void GET_BEER_TEST(){
         int beerId = 1;
-      Beer beer =  sut.getBeer(B);
+      Beer beer =  sut.getBeer();
 
         Assert.assertEquals(1,beer);
     }
