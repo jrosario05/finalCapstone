5c8afa76aaaf78a47ae61b1f64623d46c250f688
Michael Doom <michael.l.doom@gmail.com>
2022-12-14 15:48:54 -0500

    brewery detials page

--- vue/src/components/BreweryDetails.vue
+++ vue/src/components/BreweryDetails.vue
@@ -5,47 +5,82 @@
         <img v-bind:src="brewery.imgUrl" />
         <div id="color-box"></div>
       </div>
+      <div class="right-panel">
       <div id="title-info">
-        <h1 id="brewery_name" >{{ brewery.breweryName}}</h1>
+        <h1 id="brewery_name">{{ brewery.breweryName }}</h1>
         <p id="description">
-          {{ brewery.description}}
+          {{ brewery.description }}
         </p>
       </div>
-    </div>
 
-    <div class="information">
-      <div class="address">
-        <p>{{brewery.streetAddress}}  {{brewery.city}},  {{brewery.state}}  {{brewery.zip}}</p>
-      </div>
-      <div class="hours">
-        <p>Monday - {{brewery.hours.monOpen}} - {{brewery.hours.monClose}}</p>
-        <p>Tuesday - {{brewery.hours.tuesOpen}} - {{brewery.hours.tuesClose}}</p>
-        <p>Wednesday - {{brewery.hours.wedOpen}} - {{brewery.hours.wedClose}}</p>
-        <p>Thursday - {{brewery.hours.thursOpen}} - {{brewery.hours.thursClose}}</p>
-        <p>Friday - {{brewery.hours.friOpen}} - {{brewery.hours.friClose}}</p>
-        <p>Saturday - {{brewery.hours.satOpen}} - {{brewery.hours.satClose}}</p>
-        <p>Sunday - {{brewery.hours.sunOpen}} - {{brewery.hours.sunClose}}</p>
+      <div class="information">
+        <div class="hours">
+          <p id="monday">
+            Monday - {{ brewery.hours.monOpen }} -
+            {{ brewery.hours.monClose }}
+          </p>
+          <p id="tuesday">
+            Tuesday - {{ brewery.hours.tuesOpen }} -
+            {{ brewery.hours.tuesClose }}
+          </p>
+          <p id="wednesday">
+            Wednesday - {{ brewery.hours.wedOpen }} -
+            {{ brewery.hours.wedClose }}
+          </p>
+          <p id="thursday">
+            Thursday - {{ brewery.hours.thursOpen }} -
+            {{ brewery.hours.thursClose }}
+          </p>
+          <p id="friday">
+            Friday - {{ brewery.hours.friOpen }} -
+            {{ brewery.hours.friClose }}
+          </p>
+          <p id="saturday">
+            Saturday - {{ brewery.hours.satOpen }} -
+            {{ brewery.hours.satClose }}
+          </p>
+          <p id="sunday">
+            Sunday - {{ brewery.hours.sunOpen }} -
+            {{ brewery.hours.sunClose }}
+          </p>
+        </div>
+        <div class="address">
+          <p id="street">
+            {{ brewery.streetAddress }}
+          </p>
+          <p id="city">
+            {{ brewery.city }}, {{ brewery.state }} {{ brewery.zip }}
+          </p>
+        </div>
       </div>
 
       <div class="contact">
-        <div id="phone"><a v-bind:href="`tel:brewery.phoneNumber`">Call</a></div>
+        <div id="phone">
+          <a v-bind:href="`tel:brewery.phoneNumber`">Call</a>
+        </div>
         <div id="website">
           <a v-bind:href="brewery.website">Visit</a>
         </div>
       </div>
     </div>
+    </div>
 
-  <div class="beer-container">
-      <beer-card v-for="beer in beers" 
-      v-bind:key="beer.id"  
-      v-bind:beer="beer"
+    <div class="spacer">
+      <p>DISCOVER OUR BEERS</p>
+    </div>
+
+    <div class="beer-container">
+      <beer-card
+        v-for="beer in beers"
+        v-bind:key="beer.id"
+        v-bind:beer="beer"
       />
-  </div>
+    </div>
   </div>
 </template>
 
 <script>
-import BeerService from '../services/BeerService';
+import BeerService from "../services/BeerService";
 import BreweryService from "../services/BreweryService";
 import BeerCard from "./BeerCard.vue";
 
@@ -59,39 +94,37 @@ export default {
       beers: [],
     };
   },
-  created(){
-      BreweryService.breweryById(this.$route.params.id).then((response)=>{
-        this.brewery=response.data;
-      });
-      BeerService.beerByBrewery(this.$route.params.id).then(response =>{
-            this.beers=response.data;
-        });
-
-    },
-
+  created() {
+    BreweryService.breweryById(this.$route.params.id).then((response) => {
+      this.brewery = response.data;
+    });
+    BeerService.beerByBrewery(this.$route.params.id).then((response) => {
+      this.beers = response.data;
+    });
+  },
 };
 </script>
 
 <style scoped>
 #background {
-   background: url("https://www.patriot-place.com/wp-content/uploads/2019/09/Wormtown-beer-garden-hero-bg.jpg") no-repeat center center fixed; 
+  background: url("https://www.patriot-place.com/wp-content/uploads/2019/09/Wormtown-beer-garden-hero-bg.jpg")
+    no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
   height: 100%;
   background-color: black;
-
 }
 
-
 .beer-container {
-    display:flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    width: 65vw;
-    margin: 0 auto;
-    background-color: transparent !important;}
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  width: 65vw;
+  margin: 0 auto;
+  background-color: transparent !important;
+}
 
 .brewery_detail {
   display: grid;
@@ -102,7 +135,7 @@ export default {
   box-shadow: 0px 8px 12px 0px rgba(0, 0, 0, 0.75);
 }
 
-#title-info {
+.right-panel {
   grid-area: title-info;
   padding-left: 50px;
   display: flex;
@@ -111,8 +144,12 @@ export default {
   background: rgba(0, 0, 0, 0.6);
   color: white;
 }
+#title-info{
+  transform: translate(0px, -50px);
+}
 
 #title-info h1 {
+  font-size: 4em;
   text-shadow: 6px 6px 6px #272727;
 }
 
@@ -138,56 +175,74 @@ export default {
 #description {
   margin: 0;
   margin-top: 10px;
+  margin-bottom: 20px;
+  font-size: 1.5em;
 }
 
-.information {
+.spacer {
   text-shadow: 1px 1px 1px #000000;
-  padding: 0px 0px 10px 0px;
-  background-color: rgb(167, 167, 167, 0.6);
+  padding: 0px 0px 0px 0px;
+  background-color: rgba(0, 0, 0, 0.815);
   width: 100%;
-  display: grid;
-  grid-template-rows: 1fr 1fr 1fr;
-  grid-template-areas:
-    "address"
-    "hours"
-    "contact";
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.spacer p {
+  font-size: 2.5em;
+  margin: 20px;
 }
 
 .address {
   grid-area: address;
-  font-size: 1.45em;
+  font-size: 1.3em;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
-  margin-top: 10px;
-  margin-bottom: -30px;
+}
+
+.address p {
+  margin: 5px;
+}
+.information {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: "hours address";
+  transform: translate(0px, 50px);
 }
 
 .hours {
-  display: flex;
-  flex-direction: columns;
+  /* display: flex;
+  flex-direction: column;
   justify-content: space-around;
-  align-items: center;
+  align-items: center; */
   grid-area: hours;
+  margin-top: 15px;
+  font-size: 0.9em;
+}
+.hours p {
   margin: 0;
-  font-size: 0.75em;
+  margin-top: 5px;
 }
 
 .contact {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-areas: ". phone website .";
-  margin: 0px 20px;
-  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 100%;
+  transform: translate(0px, 90px);
 }
 
 #phone {
-  grid-area: phone;
   display: flex;
   align-content: center;
   justify-content: center;
-  margin-right: 25px;
-  margin-left: 50px;
+}
+#website {
+  display: flex;
+  align-content: center;
+  justify-content: center;
 }
 
 #phone a {
@@ -212,12 +267,9 @@ export default {
   background-image: linear-gradient(160deg, #d8e7e2 0%, #95a19d 100%);
 }
 #website {
-  grid-area: website;
   display: flex;
   align-content: center;
   justify-content: center;
-  margin-right: 50px;
-  margin-left: 25px;
 }
 
 #website a {
