6a3e93f886b01d041f85fc44f3fde039f607378c
Joel Rosario <aurelioros05@gmail.com>
2022-12-15 10:55:12 -0500

    pulling

--- vue/src/components/HomeCard.vue
+++ vue/src/components/HomeCard.vue
@@ -8,11 +8,9 @@
           COLUMBUS WITH YOUR PASSPORT!
         </h3>
         <p id="header-text">Discover new breweries throughout Columbus</p>
-
         <div id="splash-button">Get Your Passport</div>
       </div>
     </div>
-
     <div class="welcome">
       <h1>Welcome</h1>
       <p>
@@ -23,10 +21,7 @@
       <p>Explore local breweries, get stamps, enjoy beer.</p>
       <p id="strong">Cheers!</p>
     </div>
-
     <div class="tri-fold">
-
-
         <div class="tri-fold-element" id="first-image">
           <img src="../resources/passport.png" />
           <h3 class="title">Get Your Passport</h3>
@@ -36,8 +31,6 @@
           </p>
           <button class="bottom-button">Passport</button>
         </div>
-
-
         <div class="tri-fold-element" id="second-image">
           <img src="../resources/huge-kegs.png" />
           <h3 class="title">Explore our Partner Breweries</h3>
@@ -47,9 +40,6 @@
           </p>
           <button class="bottom-button">View Breweries</button>
         </div>
-
-
-
         <div class="tri-fold-element" id="third-image">
           <img src="../resources/slave-pouring.png" />
           <h3 class="title">Enjoy Responsibly</h3>
@@ -59,42 +49,12 @@
           </p>
           <button class="bottom-button">View Beer List</button>
         </div>
-
-
     </div>
-    <div id="app">
-
-  <transition name="fade" appear>
-    <div class="modal-overlay" 
-         v-if="showModal" 
-         @click="showModal = false"></div>
-  </transition>
-  <transition name="pop" appear>
-    <div class="modal" 
-         role="dialog" 
-         v-if="showModal"
-         >
-      <h1>Vue Transitions</h1>
-      <p>The <code>&lt;transition&gt;</code> component in Vue can create wonderful animated entrances and exits.</p>
-      <button @click="showModal = false" class="button">Hide Modal</button>
-
-    </div>
-  </transition>
-</div>
-
   </div>
 </template>
-
 <script>
-export default {
-  data(){
-    return{
-      showModal: true
-    }
-  }
-};
+export default {};
 </script>
-
 <style scoped>
 #splash-screen {
   background-color: #000000;
@@ -111,7 +71,6 @@ footer {
   color: white;
   padding: 15px;
 }
-
 #splash-screen img {
   width: 100%;
   height: 500px;
@@ -119,29 +78,25 @@ footer {
   object-position: center top;
   opacity: 0.8;
 }
-
 .splash-text {
   position: absolute;
 }
-
 #main-text {
   font-family: serif;
   color: white;
   font-size: 3em;
   margin: 0;
   text-align: center;
-  text-shadow: 0.5px 0.5px 0.25px #8d8d8d;
+  text-shadow: 0.5px 0.5px 0.25px #8D8D8D;
 }
-
 #header-text {
   color: white;
   font-size: 2em;
   text-align: center;
   padding: 0;
   margin: 10px 0px 35px;
-  text-shadow: 0.5px 0.5px 0.25px #8d8d8d;
+  text-shadow: 0.5px 0.5px 0.25px #8D8D8D;
 }
-
 #splash-button {
   width: 300px;
   height: 50px;
@@ -156,7 +111,6 @@ footer {
   margin: 0 auto;
   box-shadow: 12px 6px 10px 0px black;
 }
-
 .welcome {
   background-color: white;
   color: #809766;
@@ -165,14 +119,12 @@ footer {
   justify-content: center;
   align-items: center;
 }
-
 .welcome h1 {
   font-size: 3em;
   font-family: serif;
   margin-top: 25px;
   margin-bottom: 15px;
 }
-
 .welcome p {
   width: 50%;
   font-size: 1.5em;
@@ -180,35 +132,29 @@ footer {
   margin-top: 0px;
   margin-bottom: 10px;
 }
-
 #strong {
   margin-top: 20px;
   font-weight: bolder;
   font-size: 2em;
 }
-
 .tri-fold {
 background-color: #333232;
 display: flex;
 flex-direction: row;
 justify-content: space-evenly
 }
-
 .tri-fold-element {
     margin-top: 35px;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
 }
 .tri-fold-element img {
   width: 300px;
   height: 300px;
   object-fit: contain;
 }
-
 .title {
   color: white;
   text-align: center;
@@ -216,17 +162,13 @@ justify-content: space-evenly
   margin-top: 10px;
   margin-bottom: 6px;
 }
-
 .description {
   color: white;
   margin-top: 5px;
   margin-bottom: 0px;
   text-align: center;
-  
   font-size: 1em;
-
 }
-
 .bottom-button {
   width: 150px;
   height: 35px;
@@ -236,80 +178,5 @@ justify-content: space-evenly
   background-color:  #809766;
   border: 1px solid white;
 }
-
-
-.button {
-  border: none;
-  color: #FFF;
-  background: #42b983;
-  appearance: none;
-  font: inherit;
-  font-size: 1.8rem;
-  padding: .5em 1em;
-  border-radius: .3em;
-  cursor: pointer;
-}
-
-.modal {
-  position: absolute;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
-  text-align: center;
-  width: fit-content;
-  height: fit-content;
-  max-width: 22em;
-  padding: 2rem;
-  border-radius: 1rem;
-  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
-  background: #FFF;
-  z-index: 999;
-  transform: none;
-}
-.modal h1 {
-  margin: 0 0 1rem;
-}
-
-.modal-overlay {
-  content: '';
-  position: absolute;
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 998;
-  background: #2c3e50;
-  opacity: 0.6;
-  cursor: pointer;
-}
-
-/* ---------------------------------- */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .4s linear;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.pop-enter-active,
-.pop-leave-active {
-  transition: transform 0.4s cubic-bezier(0.5, 0, 0.5, 1), opacity 0.4s linear;
-}
-
-.pop-enter,
-.pop-leave-to {
-  opacity: 0;
-  transform: scale(0.3) translateY(-50%);
-}
-
-
 </style>
-
 
